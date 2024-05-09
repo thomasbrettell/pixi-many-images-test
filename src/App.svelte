3a963c284063e915a0f9ce59e8b6fb90a7c343ea
Brettell <thomas.a.brettell@gmail.com>
2024-05-09 16:54:13 +1000

    init

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { onMount } from "svelte";
+  import { initSketch } from "./sketch";
+
+  let mountRef;
+
+  onMount(() => {
+    initSketch(mountRef);
+  });
+</script>
+
+<div bind:this={mountRef} />
+
+<style>
+  div {
+    overflow: hidden;
+    display: flex;
+  }
+</style>
